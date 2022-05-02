2dbbcdfafaa199ddd2efe16f3dd9794c50f4f4b8
Yosafat Tambun <yosafathtambun@gmail.com>
2022-05-03 00:56:41 +0700

    Update ujiantoefl.blade.php

--- resources/views/pages/layanan/ujiantoefl.blade.php
+++ resources/views/pages/layanan/ujiantoefl.blade.php
@@ -3,67 +3,76 @@
         <div class="content-body">
             <div class="col-12">
                 <div class="card">
-                    <img src="{{ asset('app-assets/images/banner/banner-12.jpg') }}" class="img-fluid card-img-top" alt="Blog Detail Pic">
+                    <img src="{{ asset('app-assets/images/banner/banner-2.jpg') }}" class="img-fluid card-img-top" alt="Blog Detail Pic">
                     <div class="card-body">
                         <center><h1>Ujian TOEFL</h1><br></center>
+                        <h4 class="mb-75">TOEFL Institutional Test (TOEFL ITP) in IT Del Campus</h4>
                         <p class="card-text mb-2">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In efficitur tortor vitae aliquam facilisis. Mauris accumsan elit
-                            a augue posuere, ac semper ligula faucibus. In hac habitasse platea dictumst. Nunc eget laoreet ipsum, in viverra mauris.
-                            Donec blandit massa sed facilisis consequat. Proin dignissim fermentum velit vitae ornare. Fusce eu ultricies nunc. 
-                            Quisque facilisis ligula vel felis dignissim, eu mollis magna vestibulum. Vestibulum aliquet mi et ipsum tempus, vitae 
-                            viverra justo feugiat. Proin laoreet nisi porta ultrices vulputate. Nam non tempor orci, quis laoreet nisi. Etiam 
-                            venenatis vitae tellus at faucibus. Duis dignissim augue rhoncus, gravida purus sed, tempor justo. Phasellus nec mauris 
-                            eget felis finibus maximus. Curabitur a eros sodales odio consectetur tristique a a sem. Integer dapibus sodales orci, sit 
-                            amet auctor felis commodo eu.<br><br>
-                            Nunc volutpat hendrerit arcu. Suspendisse interdum iaculis dolor quis mollis. Nulla consequat mi nec laoreet pellentesque. 
-                            Nullam egestas non diam eget fermentum. Sed tellus nibh, vulputate vitae mi nec, finibus dictum felis. Donec ac venenatis 
-                            enim. Integer non risus ut dui viverra lobortis. Proin iaculis feugiat libero porttitor egestas. Phasellus hendrerit 
-                            luctus nibh sed blandit. Donec dignissim consectetur nisl, vestibulum pulvinar sem aliquam ac. Aenean tincidunt nisl nisl, 
-                            non aliquet dolor mollis ac. Nullam porttitor risus at lorem semper porta. Curabitur varius quam ac mauris ornare 
-                            dignissim. Nunc in erat mauris. Curabitur dignissim risus odio, at sagittis ex feugiat quis.<br><br>
-                            Integer justo urna, commodo ut fringilla a, sollicitudin at leo. Pellentesque fringilla dapibus tellus, id congue mi 
-                            hendrerit volutpat. Quisque bibendum nibh enim, quis dignissim elit egestas sed. Integer laoreet dolor erat, vulputate 
-                            feugiat elit gravida non. Sed bibendum in purus at iaculis. Donec ac urna quis massa congue scelerisque. Vivamus ut purus 
-                            et felis vulputate porta. Morbi vulputate, odio ac egestas finibus, quam quam varius sem, ut bibendum velit mi vitae purus.<br>
-                            Nam commodo, ligula volutpat eleifend ultrices, erat sapien elementum mauris, eget condimentum orci neque sit amet nibh. 
-                            Aenean sed lacus mauris. Nullam porttitor mauris at sollicitudin porttitor. Integer ultricies leo ultricies, hendrerit 
-                            quam nec, vehicula sem. Integer justo enim, scelerisque sit amet interdum eu, suscipit eu eros. Praesent in hendrerit sem. 
-                            Quisque semper leo dui, sed consequat felis accumsan vel.<br><br>
-                            Phasellus vulputate volutpat enim, vel malesuada metus fringilla eget. Nam laoreet leo ac sem viverra, id dignissim velit 
-                            porttitor. In porttitor blandit lacus at venenatis. Nullam a mi in tortor venenatis vulputate quis vel lorem. Pellentesque 
-                            risus ipsum, imperdiet sed nunc venenatis, rutrum varius erat. Donec erat leo, pulvinar eget quam eu, tincidunt venenatis 
-                            est. Etiam hendrerit porta placerat. Morbi sit amet sapien eget dui mollis imperdiet. Donec massa enim, egestas ac porta 
-                            ut, finibus nec nibh. Nullam in tempus nisl.
+                            IT Del has administered TOEFL ITP Test (Test of English as a Foreign Language Institutional Testing Program) since 01
+                            June 2013 after signing the MoU between IT Del and IIEF (The Indonesian International Education Foundation). IIEF 
+                            operates under the supervision of ETS America. The score of TOEFL ITP given by IIEF is the score that is officially 
+                            issued by Educational Testing Service (ETS), Princeton, United States of America.
+                        </p><br>
+                        <h5 class="mb-75">TEST TIME</h5>
+                        <p class="card-text mb-2">
+                            The test is generally started from 08:00 – 11:00 a.m, and if because of some reasons the time needs to be changed, the 
+                            test coordinator will inform the participants individually in advance.
+                        </p><br>
+                        <h5 class="mb-75">TEST FEE</h5>
+                        <p class="card-text mb-2">
+                            The test is IDR. 570.000 (five hundred seventy thousand rupiahs). for each candidate. If you wish the test result to 
+                            be sent to your postal address, you will be charged an additional fee that you will pay on the test date. The amount 
+                            of additional fee is specified based on the cost delivery standard made by IT Del.
+                            Transfer payment to the Bank account appointed by IT Del:<br>
+                            <b>Bank BNI Cabang Balige<br>
+                            Account name: LPPM PI Del<br>
+                            Account number: 0187092089<br>
+                            With subject: TOEFL ITP registration fee on behalf of (mention your name)<br></b>
+                        </p><br>
+                        <h5 class="mb-75">HOW TO REGISTER?</h5>
+                        <p class="card-text mb-2">
+                            Registration can be done via phone call at 0632-331234 or via facsimile at 0632-331116 or via email to monalisa.pasaribu@del.ac.id by
+                            completing the registration form that covers:
+                            <ul class="p-0 mb-2">
+                                <li class="d-block">
+                                    <span class="me-25">1</span>
+                                    <span>The applicants name</span>
+                                </li>
+                                <li class="d-block">
+                                    <span class="me-25">2</span>
+                                    <span>Preferred test date</span>
+                                </li>
+                                <li class="d-block">
+                                    <span class="me-25">3</span>
+                                    <span>The copy of passport or citizen card used for registering</span>
+                                </li>
+                                <li class="d-block">
+                                    <span class="me-25">4</span>
+                                    <span>Transfer payment receipt</span>
+                                </li>
+                                <li class="d-block">
+                                    <span class="me-25">5</span>
+                                    <span>TOEFL ITP result collection preference</span>
+                                </li>
+                            </ul>
+                        </p><br>
+                        <h5 class="mb-75">TOEFL ITP MATERIALS</h5>
+                        <p class="card-text mb-2">
+                            1. The TOEFL ITP test is conducted online and/or onsite.<br>
+                            2. The test standard and the assessment are similar to Paper-Based System International TOEFL.<br>
+                            3. The test form is multiple choice.<br>
+                            4. The TOEFL ITP sections are:<br>
+                            - Section 1: Listening Comprehension (50 items)<br>
+                            - Section 2: Structure and Written Expressions (40 items)<br>
+                            - Section 3: Reading Comprehension (50 items)<br>
+                        </p><br>
+                        <h5 class="mb-75">TOEFL ITP Result</h5>
+                        <p class="card-text mb-2">
+                            All tests are provided by IIEF Jakarta. The score of TOEFL ITP given by IIEF is the score that is officially issued by 
+                            Educational Testing Service (ETS), Princeton, United States of America.<br><br>The TOEFL ITP result will be available and 
+                            can be directly taken at IT Del campus 3 (three) weeks after the test date. This is due to the process of answer sheet 
+                            delivery from IT Del to IIEF and the certificate delivery from IIEF to IT Del.
                         </p>
-                        <h4 class="mb-75">Unprecedented Challenge</h4>
-                        <ul class="p-0 mb-2">
-                            <li class="d-block">
-                                <span class="me-25">-</span>
-                                <span>Preliminary thinking systems</span>
-                            </li>
-                            <li class="d-block">
-                                <span class="me-25">-</span>
-                                <span>Bandwidth efficient</span>
-                            </li>
-                            <li class="d-block">
-                                <span class="me-25">-</span>
-                                <span>Green space</span>
-                            </li>
-                            <li class="d-block">
-                                <span class="me-25">-</span>
-                                <span>Social impact</span>
-                            </li>
-                            <li class="d-block">
-                                <span class="me-25">-</span>
-                                <span>Thought partnership</span>
-                            </li>
-                            <li class="d-block">
-                                <span class="me-25">-</span>
-                                <span>Fully ethical life</span>
-                            </li>
-                        </ul>
-                        <hr class="my-2">
-    
                         </div>
                     </div>
                 </div>
