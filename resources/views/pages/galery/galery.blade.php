d42107bab909f2912010a573895ccb22db20a434
juliantisi <juliantisitorus071@gmail.com>
2022-05-04 15:54:43 +0700

    update

--- resources/views/pages/galery/galery.blade.php
+++ resources/views/pages/galery/galery.blade.php
@@ -1,3 +1,25 @@
 <x-web-layout title="Galery">
-
+    <!-- BEGIN: Content-->
+   <div class="app-content content ">
+       <div class="content-body">
+           <div id="user-profile">
+               <!-- profile header -->
+               <div class="row">
+                    <div class="col-xl-4 col-lg-5 col-md-5 order-1 order-md-0">
+                        <div class="card">
+                            <img class="card-img-top" src="../../../app-assets/images/slider/04.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title">Card title</h4>
+                                <p class="card-text">
+                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </p>
+                                <a href="#" class="btn btn-outline-primary waves-effect">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+       </div>
+   </div>
+    <!-- /User Card -->
 </x-web-layout>
