1f2eaa6faa288860d33ebfa5bfa5cee543eb1ae5
Yosafat Tambun <yosafathtambun@gmail.com>
2022-05-06 14:25:11 +0700

    new

--- resources/views/pages/requestruangan/main.blade.php
+++ resources/views/pages/requestruangan/main.blade.php
@@ -43,7 +43,7 @@
                               <td valign="top" class="dataTables_empty">{{ $item->time_end }}</td>
                               <td valign="top" class="dataTables_empty">{{ $item->description }}</td>
                               <td>
-                              <div class="d-inline-flex">
+                                <div class="d-inline-flex">
                                     <a class="pe-1 dropdown-toggle hide-arrow text-primary" data-bs-toggle="dropdown" aria-expanded="false">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical font-small-4">
                                             <circle cx="12" cy="12" r="1"></circle>
