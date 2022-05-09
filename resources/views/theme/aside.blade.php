8f9c8d4e2d353cf6c1c1688d4931f1bf861223e7
Yosafat Tambun <yosafathtambun@gmail.com>
2022-05-09 20:30:20 +0700

    Update aside.blade.php

--- resources/views/theme/aside.blade.php
+++ resources/views/theme/aside.blade.php
@@ -2,7 +2,7 @@
 <div class="main-menu menu-fixed menu-light menu-accordion menu-shadow" data-scroll-to-active="true">
     <div class="navbar-header">
       <ul class="nav navbar-nav flex-row">
-        <li class="nav-item me-auto"><a class="navbar-brand" href="{{ route('dashboard') }}"><span class="brand-logo">
+        <li class="nav-item me-auto"><a class="navbar-brand" href="{{ route('background') }}"><span class="brand-logo">
               <img src="{{ asset('img/Logo.jpg') }}"></span>
             <h2 class="brand-text">UPT Bahasa</h2></a></li>
         <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pe-0" data-bs-toggle="collapse"><i class="d-block d-xl-none text-primary toggle-icon font-medium-4" data-feather="x"></i><i class="d-none d-xl-block collapse-toggle-icon font-medium-4  text-primary" data-feather="disc" data-ticon="disc"></i></a></li>
