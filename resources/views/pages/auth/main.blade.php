5f9838b5b8dd085b43adc7825db429566d07a765
Yosafat Tambun <yosafathtambun@gmail.com>
2022-05-10 20:23:55 +0700

    1

--- resources/views/pages/auth/main.blade.php
+++ resources/views/pages/auth/main.blade.php
@@ -4,8 +4,8 @@
 			<form class="form w-100" novalidate="novalidate" id="form_login">
 				<div class="text-center mb-10">
 					 <h1 class="text-dark mb-3">{{config('app.name')}}</h1> 
-					<div class="text-gray-400 fw-bold fs-4">Belum Memiliki Akun?
-					<a href="javascript:;" onclick="auth_content('page_register');" class="link-primary fw-bolder">Buat Akun</a></div>
+					<div class="text-gray-400 fw-bold fs-4">Don't have an account yet?
+					<a href="javascript:;" onclick="auth_content('page_register');" class="link-primary fw-bolder">Register</a></div>
 				</div>
 				<div class="fv-row mb-10">
 					<label class="form-label fs-6 fw-bolder text-dark">Username</label>
@@ -19,7 +19,7 @@
 				</div>
 				<div class="text-center">
 					<button type="submit" id="tombol_login" onclick="handle_post('#tombol_login','#form_login','{{route('auth.login')}}');" class="btn btn-lg btn-primary w-100 mb-5">
-						<span class="indicator-label">Continue</span>
+						<span class="indicator-label">Login</span>
 						<span class="indicator-progress">Please wait...
 						<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
 					</button>
@@ -31,8 +31,8 @@
         <div class="w-lg-600px bg-body rounded shadow-sm p-10 p-lg-15 mx-auto">
             <form class="form w-100" novalidate="novalidate" id="form_register">
                 <div class="mb-10 text-center">
-                    <h1 class="text-dark mb-3">Buat Akun</h1>
-                    <div class="text-gray-400 fw-bold fs-4">Sudah memiliki akun?
+                    <h1 class="text-dark mb-3">Register</h1>
+                    <div class="text-gray-400 fw-bold fs-4">Already have an account?
                     <a href="javascript:;" onclick="auth_content('page_login');" class="link-primary fw-bolder">Log in here</a></div>
                 </div>
                 <div class="row fv-row mb-7">
@@ -49,7 +49,7 @@
                 </div>
                 <div class="text-center">
                     <button type="button" id="tombol_register" onclick="handle_post('#tombol_register','#form_register','{{route('auth.register')}}');" class="btn btn-lg btn-primary">
-                        <span class="indicator-label">Submit</span>
+                        <span class="indicator-label">Register</span>
                         <span class="indicator-progress">Please wait...
                         <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
                     </button>
