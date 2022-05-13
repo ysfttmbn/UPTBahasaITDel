5913a9bb99d85f342af5ee976370dbdacbb134fc
juliantisi <juliantisitorus071@gmail.com>
2022-05-13 13:59:22 +0700

    update

--- resources/views/theme/aside.blade.php
+++ resources/views/theme/aside.blade.php
@@ -83,11 +83,7 @@
               
         <li class=" nav-item">
           <a class="d-flex align-items-center" href="{{ route('inventory.index') }}">
-<<<<<<< HEAD
             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
-=======
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-archive"><polyline points="21 8 21 21 3 21 3 8"></polyline><rect x="1" y="3" width="22" height="5"></rect><line x1="10" y1="12" x2="14" y2="12"></line></svg>
->>>>>>> aa433e21741202a5d0ee8528c0fde063b31dc3bb
             <span class="menu-title text-truncate" data-i18n="Raise Support">Inventory</span>
           </a>
         </li>
@@ -105,13 +101,6 @@
           <span class="menu-title text-truncate" data-i18n="File Manager">Users</span>
         </a>
      </li>
-
-      <li class=" nav-item">
-        <a class="d-flex align-items-center" href="{{ route('inventory.index') }}">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-          <span class="menu-title text-truncate" data-i18n="Raise Support">Users</span>
-        </a>
-      </li>
       </ul>
     </div>
   </div>
